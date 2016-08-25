67b00d4a97a65f1e421570316344d5f569bb364f
Tyler Sedlar <sedlarizona@gmail.com>
2016-08-24 17:27:02 -0700

    Resize panorama and add favicon

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const path = require('path')
+const favicon = require('serve-favicon')
 
 const server = express()
 
 let rel = (file) => path.join(__dirname, file)
 
+server.use(favicon(rel('./favicon.ico')))
 server.use(require('connect-livereload')())
 server.use(express.static(rel('.')))
 server.use(express.static('dist/public'))
